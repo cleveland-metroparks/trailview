5bb80841e5656eb94df8722f87bbcb0e28aecdd5
orosmatthew <orosmatthew@pm.me>
2022-08-15 15:38:44 -0400

    Improve import page

--- public/admin/js/import.js
+++ public/admin/js/import.js
@@ -1,58 +1,61 @@
 
-let prevVal = "";
-document.getElementById('name_input').addEventListener('input', function(e){
-  if(this.checkValidity()){
-    prevVal = this.value;
-  } else {
-    this.value = prevVal;
-  }
-});
-
+// Uploader object
 var uploader = new plupload.Uploader({
-    browse_button: 'browse', // this can be an id of a DOM element or the DOM element itself
+    browse_button: 'browse', // Id of element
     url: '/admin/api/upload.php',
 });
-   
+var prevNameVal = "";   // Previous name input value (used for validation)
+var failed = false; // If upload failed
+var uploading = false; // If currently uploading
+
+
+$('#name_input').on('input', () => {
+    if (this.checkValidity()) {
+        prevNameVal = this.value;
+    } else {
+        this.value = prevNameVal;
+    }
+});
+
 uploader.init();
 
-uploader.bind('FilesAdded', function(up, files) {
+uploader.bind('FilesAdded', function (up, files) {
     var html = '';
-    plupload.each(files, function(file) {
-      html += '<li id="' + file.id + '">' + file.name + ' (' + plupload.formatSize(file.size) + ') <b></b></li>';
+    plupload.each(files, function (file) {
+        html += '<li id="' + file.id + '">' + file.name + ' (' + plupload.formatSize(file.size) + ') <b></b></li>';
     });
     document.getElementById('filelist').innerHTML += html;
-  });
+});
 
-  uploader.bind('UploadProgress', function(up, file) {
+uploader.bind('UploadProgress', function (up, file) {
     document.getElementById(file.id).getElementsByTagName('b')[0].innerHTML = '<span>' + file.percent + "%</span>";
-  });
+});
 
-uploader.bind('Error', function(up, err) {
+uploader.bind('Error', function (up, err) {
     document.getElementById('console').innerHTML += "Error #" + err.code + ": " + err.message;
-  });
+});
 
-uploader.bind('FileUploaded', function(up, file, result) {
+uploader.bind('FileUploaded', function (up, file, result) {
     document.getElementById('console').innerHTML += result.response;
 });
 
-
-uploader.bind('UploadComplete', function(up, files) {
+uploader.bind('UploadComplete', function (up, files) {
     let data = {
         'name': document.getElementById('name_input').value,
         'status': 'Blur',
     };
     $.post('/admin/api/set_status.php', data);
     document.getElementById('console').innerHTML += "<strong>DONE! You can now close this window!</strong>\n";
+    window.onbeforeunload = function () {
+        return;
+    };
 });
 
-var failed = false;
-var uploading = false;
-
-document.getElementById('submit_button').onclick = function() {
+$('#submit_button').on('click', () => {
     if (failed == true || uploading == true) {
         return;
     }
-    window.onbeforeunload = function() {
+    window.onbeforeunload = function () {
         return true;
     };
     if (uploader.files.length == 0) {
@@ -68,7 +71,7 @@ document.getElementById('submit_button').onclick = function() {
         'name': document.getElementById('name_input').value,
     };
     $.post('/admin/api/insert_trail.php', data)
-        .done(function(data) {
+        .done(function (data) {
             if (data == 'exists') {
                 failed = true;
                 document.getElementById('console').innerHTML += "Error: Trail already exists, not importing!\n";
@@ -82,4 +85,4 @@ document.getElementById('submit_button').onclick = function() {
     })
     uploader.start();
     uploading = true;
-  };
+});
