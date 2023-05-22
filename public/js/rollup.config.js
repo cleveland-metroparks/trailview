3a6def2d16247929f0078a7f4883d4c051fa8f2c
orosmatthew <orosmatthew@pm.me>
2023-05-22 11:22:35 -0400

    Add node

---
+++ public/js/rollup.config.js
@@ -0,0 +1,15 @@
+import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+
+export default {
+    input: 'js/src/trailviewer.ts',
+    output: {
+        file: 'js/dist/bundle.js',
+        format: 'iife',
+    },
+    plugins: [resolve(), commonjs(), typescript({
+        compilerOptions: { "target": "ES2022", "strict": true, "moduleResolution": "nodenext" }
+    }), terser()]
+};
