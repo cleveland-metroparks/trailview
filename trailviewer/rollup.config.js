93628e26f92b70a7c7601dd108461838e0165c45
Matthew Oros <mjo1@clevelandmetroparks.com>
2023-05-26 09:52:05 -0400

    [trailviewer] ES module uses external dependencies

--- trailviewer/rollup.config.js
+++ trailviewer/rollup.config.js
@@ -4,22 +4,43 @@ import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
 import css from 'rollup-plugin-import-css';
 
-export default {
-    input: './src/trailviewer.ts',
-    output: [{
-        name: 'trailviewer',
-        file: './dist/trailviewer.umd.js',
-        format: 'umd',
+export default [
+    {
+        input: './src/trailviewer.ts',
+        external: [
+            'cheap-ruler',
+            'mapbox-gl',
+            'events',
+            'url-join',
+            'pannellum',
+        ],
+        output: {
+            file: './dist/trailviewer.mjs',
+            format: 'es',
+        },
+        plugins: [
+            resolve({ preferBuiltins: false }),
+            commonjs(),
+            typescript(),
+            terser(),
+            css({ minify: true, output: './dist/trailviewer.css' }),
+        ],
     },
     {
-        file: './dist/trailviewer.mjs',
-        format: 'es'
-    }],
-    plugins: [
-        resolve({ preferBuiltins: false }),
-        commonjs(),
-        typescript(),
-        terser(),
-        css({ minify: true, output: './dist/trailviewer.css' }),
-    ],
-};
+        input: './src/trailviewer.ts',
+        output: {
+            name: 'trailviewer',
+            file: './dist/trailviewer.umd.js',
+            format: 'umd',
+        },
+        plugins: [
+            resolve({ preferBuiltins: false }),
+            commonjs(),
+            typescript(),
+            terser(),
+            // I am aware that the css is built twice, but working
+            // around that would be more complicated than it's worth
+            css({ minify: true, output: './dist/trailviewer.css' }),
+        ],
+    },
+];
