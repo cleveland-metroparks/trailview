0f054ab2d7a6956aeb3cb615b1885a269cc8d568
orosmatthew <mjo1@clevelandmetroparks.com>
2024-05-30 11:47:21 -0400

    Disable security headers for now

--- web/src/hooks.server.ts
+++ web/src/hooks.server.ts
@@ -1,13 +1,13 @@
 import type { Handle } from '@sveltejs/kit';
 
-function appendSecurityHeaders(res: Response) {
-	// Security-related headers
-	// CSP related headers are set in svelte.config.js
-	res.headers.append('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
-	res.headers.append('X-Content-Type-Options', 'nosniff');
-	res.headers.append('Referrer-Policy', 'same-origin');
-	res.headers.append('X-XSS-Protection', '1; mode=block');
-}
+// function appendSecurityHeaders(res: Response) {
+// 	// Security-related headers
+// 	// CSP related headers are set in svelte.config.js
+// 	res.headers.append('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+// 	res.headers.append('X-Content-Type-Options', 'nosniff');
+// 	res.headers.append('Referrer-Policy', 'same-origin');
+// 	res.headers.append('X-XSS-Protection', '1; mode=block');
+// }
 
 export const handle = (async ({ event, resolve }) => {
 	if (event.url.pathname.startsWith('/api')) {
@@ -23,10 +23,10 @@ export const handle = (async ({ event, resolve }) => {
 		const res = await resolve(event);
 		res.headers.append('Access-Control-Allow-Origin', '*');
 		res.headers.append('Access-Control-Allow-Headers', '*');
-		appendSecurityHeaders(res);
+		// appendSecurityHeaders(res);
 		return res;
 	}
 	const res = await resolve(event);
-	appendSecurityHeaders(res);
+	// appendSecurityHeaders(res);
 	return res;
 }) satisfies Handle;
