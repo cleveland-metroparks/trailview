5272356b59c1ee06003c68a39c8b6adcb2448c2c
orosmatthew <mjo1@clevelandmetroparks.com>
2024-06-07 12:41:58 -0400

    Improve sequence table

--- web/src/routes/admin/sequence-list/+page.svelte
+++ web/src/routes/admin/sequence-list/+page.svelte
@@ -61,54 +61,57 @@
 		<input bind:checked={showDone} id="showDoneSwitch" type="checkbox" class="form-check-input" />
 		<label for="showDoneSwitch">Show Done</label>
 	</div>
-	<table class="table table-bordered">
-		<thead>
-			<tr>
-				<th>Id</th>
-				<th>Name</th>
-				<th>Status</th>
-				<th>Actions</th>
-			</tr>
-		</thead>
-		<tbody>
-			{#each tableData as sequence}
+	<div class="table-responsive">
+		<table class="table table-bordered table-hover">
+			<thead>
 				<tr>
-					<td>{sequence.id}</td>
-					<td>{sequence.name}</td>
-					{#if sequence.toDelete === false}
-						<td class="status-text"
-							><span class={`badge bg-${statusColor(sequence.status)}`}>{sequence.status}</span></td
-						>
-					{:else}
-						<td class="status-text"><span class="badge bg-danger">Delete</span></td>
-					{/if}
-					<td>
-						{#if sequence.toDelete === false}
-							<form
-								action="?/delete"
-								method="POST"
-								use:enhance={({ cancel }) => {
-									const response = prompt(
-										`Are you sure you want to delete ${sequence.name}? Type 'I understand' to confirm deletion.`
-									);
-									if (response !== null && response === 'I understand') {
-										return async ({ update }) => {
-											await update();
-										};
-									} else {
-										cancel();
-									}
-								}}
-							>
-								<input type="hidden" name="sequenceId" value={sequence.id} />
-								<button type="submit" class="btn btn-sm btn-danger">Delete</button>
-							</form>
-						{/if}
-					</td>
+					<th scope="col">Id</th>
+					<th scope="col">Name</th>
+					<th scope="col">Status</th>
+					<th scope="col">Actions</th>
 				</tr>
-			{/each}
-		</tbody>
-	</table>
+			</thead>
+			<tbody>
+				{#each tableData as sequence}
+					<tr>
+						<th scope="row">{sequence.id}</th>
+						<td>{sequence.name}</td>
+						{#if sequence.toDelete === false}
+							<td class="status-text"
+								><span class={`badge bg-${statusColor(sequence.status)}`}>{sequence.status}</span
+								></td
+							>
+						{:else}
+							<td class="status-text"><span class="badge bg-danger">Delete</span></td>
+						{/if}
+						<td>
+							{#if sequence.toDelete === false}
+								<form
+									action="?/delete"
+									method="POST"
+									use:enhance={({ cancel }) => {
+										const response = prompt(
+											`Are you sure you want to delete ${sequence.name}? Type 'I understand' to confirm deletion.`
+										);
+										if (response !== null && response === 'I understand') {
+											return async ({ update }) => {
+												await update();
+											};
+										} else {
+											cancel();
+										}
+									}}
+								>
+									<input type="hidden" name="sequenceId" value={sequence.id} />
+									<button type="submit" class="btn btn-sm btn-danger">Delete</button>
+								</form>
+							{/if}
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	</div>
 </div>
 
 <style>
