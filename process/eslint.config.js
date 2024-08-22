5e6224c649425acbfb5da41a3c4558a8ecebe8e1
orosmatthew <mjo1@clevelandmetroparks.com>
2024-08-22 09:04:46 -0400

    [process] Use correct type for eslint

--- process/eslint.config.js
+++ process/eslint.config.js
@@ -2,7 +2,7 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import ts from 'typescript-eslint';
 
-/** @type {import('eslint').Linter.FlatConfig[]} */
+/** @type {import('eslint').Linter.Config[]} */
 export default [
     { files: ['**/*.{js,mjs,cjs,ts}'] },
     { languageOptions: { globals: globals.node } },
